0e039e5ce78f98815f2a613050c9cf113a6c946d
olee46 <orisande5.46@gmail.com>
2018-02-23 00:54:33 +0900

    setAuthStatePersistence to SESSION: user is logged out on closing browser tab/window; enabled login with ENTER key

--- public/scripts/index-script.js
+++ public/scripts/index-script.js
@@ -10,33 +10,51 @@ var config = {
 firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    window.location.href = "/study-rooms.html";
-  } else {
-    
-  }
-})
-
-$('#sign-in').click(function() {
-  // get email & password
-  var email = $('#email').val();
-  var password = $('#password').val();
-
-  // Firebase: user sign-up
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-    window.location.href = 'study-rooms.html';
-  }).catch(function(error) {
-      var errorCode = error.code;
-
-      // clear previous errors
-      $('#error-sign-in').text();
-
-      if (errorCode == 'auth/invalid-email' || errorCode == 'auth/wrong-password') {
-        $('#error-sign-in').html('※ログインに失敗しました<br><br>');
-      } else if (errorCode == 'auth/user-not-found') {
-        $('#error-sign-in').html('※アカウントが存在しません<br>「新規登録」ボタンからアカウントを作成してください<br><br>');
-      } else if (errorCode == 'auth/user-disabled') {
-        $('#error-sign-in').html('※アカウントが停止されています<br><br>');
-      }
-    });
+    if (user) {
+      window.location.href = "/study-rooms.html";
+    } else {
+ 
+    }
 });
+
+// click "sign in" button
+$('#sign-in').click(function() {
+    signIn();
+});
+
+// press ENTER key
+$('#password').keypress(function(e) {
+    if (e.which == 13) {
+      signIn();
+    }  
+});
+
+
+
+function signIn() {
+    // get email & password
+    var email = $('#email').val();
+    var password = $('#password').val();
+
+    // Firebase: user sign-up
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function() {
+          return firebase.auth().signInWithEmailAndPassword(email, password);
+    }).then(function() {
+          window.location.href = 'study-rooms.html';
+    }).catch(function(error) {
+        var errorCode = error.code;
+
+        // clear previous errors
+        $('#error-sign-in').text();
+
+        if (errorCode == 'auth/invalid-email' || errorCode == 'auth/wrong-password') {
+          $('#error-sign-in').html('※ログインに失敗しました<br><br>');
+        } else if (errorCode == 'auth/user-not-found') {
+          $('#error-sign-in').html('※アカウントが存在しません<br>「新規登録」ボタンからアカウントを作成してください<br><br>');
+        } else if (errorCode == 'auth/user-disabled') {
+          $('#error-sign-in').html('※アカウントが停止されています<br><br>');
+        }
+      });
+
+}
