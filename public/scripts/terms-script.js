0d250b856cfebca159435473d40e6aa53a4f049a
olee46 <orisande5.46@gmail.com>
2018-04-05 16:35:04 +0900

    added terms

---
+++ public/scripts/terms-script.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDRmp_XJqP10QY0oop0Y0u7WalMhDqrhaQ",
+  authDomain: "fireba-a8775.firebaseapp.com",
+  databaseURL: "https://fireba-a8775.firebaseio.com",
+  projectId: "fireba-a8775",
+  storageBucket: "fireba-a8775.appspot.com",
+  messagingSenderId: "86072280692"
+};
+firebase.initializeApp(config);
+
+$("input[name='agree']").on('change', function() {
+	if ($(this).val() == 'yes') {
+		// enable button
+		$('#proceed').removeAttr('disabled');
+	}
+})
